8c3cca753a4436b40901f26c4e09ff7eb0997b9a
zakuni <kunio038@gmail.com>
2013-07-15 19:48:38 +0900

    deleted unnecessary codes

--- main.rb
+++ main.rb
@@ -4,13 +4,9 @@ class TimerApp < Sinatra::Base
   Slim::Engine.set_default_options :pretty => true
 
   get '/' do
-    EM.run do
-      n = 0
-      timer = EventMachine::PeriodicTimer.new(1) do
-        ## push "tick" to client        
-        io.push :tick, Time.now
-        # timer.cancel if (n+=1) > 5
-      end
+    timer = EventMachine::PeriodicTimer.new(1) do
+      ## push "tick" to client        
+      io.push :tick, Time.now
     end
     slim :index
   end
